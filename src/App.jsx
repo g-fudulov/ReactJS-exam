516ecd3ff8119a25018ae2951235fc3cdf603267
g-fudulov <fudulov.georgi@gmail.com>
2024-08-01 21:01:30 +0300

    Removed adit and delete forms for the profiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import "./App.css";
 import CreateJobForm from "./components/forms/CreateJobForm";
 import DeleteJobForm from "./components/forms/DeleteJobForm";
-import DeleteUserForm from "./components/forms/DeleteUserForm";
 import EditJobForm from "./components/forms/EditJobForm";
-import EditUserForm from "./components/forms/EditUserForm";
 import LoginForm from "./components/forms/LoginForm";
 import LogoutForm from "./components/forms/LogoutForm";
 import RegisterForm from "./components/forms/RegisterForm";
@@ -23,8 +21,6 @@ function App() {
         <Route path="/logout" element={<LogoutForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/profile/view/:profileId" element={<ProfileShow />} />
-        <Route path="/profile/edit/:profileId" element={<EditUserForm />} />
-        <Route path="/profile/delete/:profileId" element={<DeleteUserForm />} />
         {/* <Route path="/market" element={<Home />} /> */}
         <Route path="/jobs/create" element={<CreateJobForm />} />
         <Route path="/jobs/edit/:jobId" element={<EditJobForm />} />
--- src/components/forms/DeleteUserForm.jsx
+++
@@ -1,3 +0,0 @@
-export default function DeleteUserForm() {
-  return(<></>);
-}
