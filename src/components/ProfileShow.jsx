7ef0e8a4172691dfea6841c475f0f0e28b7a95dc
g-fudulov <fudulov.georgi@gmail.com>
2024-08-01 21:13:43 +0300

    Added Show Profile component

--- src/components/ProfileShow.jsx
+++ src/components/ProfileShow.jsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useAuth } from "../context/Auth";
+
 export default function ProfileShow() {
-  return(<></>);
+  const { profileId } = useParams();
+  const { currentUser } = useAuth();
+  return (
+    <main>
+      <h1>Welcome, {currentUser.firstName}</h1>
+      <section>
+        <p>Email: {currentUser.email}</p>
+        <p>First Name: {currentUser.firstName}</p>
+        <p>Second Name: {currentUser.secondName}</p>
+        <p>Country: {currentUser.location}</p>
+        <p>Joined: {new Date(currentUser._createdOn).toString()}</p>
+        <p>Public Profile ID: {profileId}</p>
+      </section>
+    </main>
+  );
 }
